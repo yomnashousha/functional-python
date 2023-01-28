a15ba7d947f5a47ffb3a6ae76c923535ff5604d6
Yomna <yshousha24@cmc.edu>
2023-01-28 10:11:19 -0800

    modified errors

--- Fixme.py
+++ Fixme.py
@@ -5,6 +5,7 @@ Your solutions should use the map and filter functions,
 and not for loops or list comprehensions.
 '''
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
@@ -20,11 +21,14 @@ def evens(n):
     >>> evens(-1)
     []
     '''
+    xs = range(n+1)
+    xs = list(filter(lambda x: x % 2 == 0, xs))
+    return xs
 
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from 0 to n inclusive that contain digit 3.
 
     >>> threes(2)
     []
@@ -37,11 +41,14 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
+    xs = range(n+1)
+    xs = list(filter(lambda x: True if "3" in str(x) else False, xs))
+    return xs
 
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 5 characters long.
+    Returns a list of all words in the text that are less than 5 char long.
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -55,6 +62,9 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
+    xs = text.split()
+    xs = list(filter(lambda x: True if len(x) < 5 else False, xs))
+    return xs
 
 
 def squares(n):
@@ -71,11 +81,14 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
+    xs = range(1, n+1)
+    xs = list(map(lambda x: x*x, xs))
+    return xs
 
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings, returns a list of the lengths of the strings.
 
     >>> lengths([])
     []
@@ -84,3 +97,5 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    strings = list(map(lambda x: len(x), strings))
+    return strings
